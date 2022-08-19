29fc8acae87d6328a1abf375a3e9e77fdf5eff5b
Degeq <gorlovoy89@mail.ru>
2022-08-19 21:21:00 +0300

    InfiniteIterator

---
+++ InfiniteIterator/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i : new Randoms(90, 100)) {
+            System.out.println("Сгенерированное число: " + i);
+            if (i == 100) {
+                System.out.println("Выпало число 100, давайте на этом закончим");
+                break;
+            }
+        }
+    }
+}
+
